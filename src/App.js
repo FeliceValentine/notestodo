18d8a39a428429c0cfb0f92b080c8bddca7d62df
FeliceValentine <felice.k.valentine@gmail.com>
2021-07-30 13:31:49 +0100

    mlist

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
+
+class App extends React.Component {
+  state = {
+    todo: [],
+    userInput: ""
+  }
+
+
+  addHandler = () => {
+    let newTodo = [...this.state.todo]
+    newTodo.push(this.state.userInput)
+    this.setState({ todo: newTodo })
+    this.setState({ userInput: "" })
+  }
+
+ removeAll = () =>{
+  let newTodo = [...this.state.todo]
+  newTodo.pop(this.state.userInput)
+  this.setState({ todo: newTodo })
+  this.setState({ userInput: "" })
+ }
+
+ remove = () =>{
+  let newTodo = [...this.state.todo]
+  newTodo.pop(this.state.userInput)
+  this.setState({ todo: newTodo })
+  this.setState({ userInput: "" })
+ }
+ 
+
+  render() {
+    console.log(this.state)
+    return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>tasks</h1>
+      {this.state.todo.map((item, index) => {
+        return (
+          <div className ="list" key={index}>
+            <h2>{item}</h2>
+            <button className="x" onClick={this.remove}>x</button>
+          </div>
+        )
+      })}
+      <input onChange={(event) => this.setState({userInput: event.target.value}) }
+        value={this.state.userInput}
+      />
+      {}
+      <button onClick={this.addHandler}>add task</button>
+      <button onClick={this.removeAll}>remove all</button>
+      
     </div>
   );
 }
-
+}
 export default App;
